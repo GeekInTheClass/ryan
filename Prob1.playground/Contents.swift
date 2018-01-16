367e1d8f0e57a07d5be8899ce25b709fd73f2184
minjujeong <rianbow369@naver.com>
2018-01-16 16:44:49 +0900

    ㅇㅇ

--- Prob1.playground/Contents.swift
+++ Prob1.playground/Contents.swift
@@ -1,3 +1,4 @@
+//문제1(이 문제 이상하면 풀지 마세요ㅠㅠ)
 let numArr1 = [1, 2, 3, 4, 6, 8, 34, 13, 33, 66, 66666, 789]
 var retArr1: [Int] = []
 var ret1: Int = 0
@@ -10,7 +11,7 @@ for i in 0..<numArr1.count{
 }
 print(retArr1)
 
-
+//문제2
 let numArr2 = [1, 2, 3, 4, 6, 8, 34, 13, 33, 66, 66666, 789]
 var ret2: Int = 0
 for i in 0..<numArr2.count{
